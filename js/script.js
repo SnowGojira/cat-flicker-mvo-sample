d35ef12c717ff32704f55ea598e71932fcc96375
Haku <hakuhal.snow@gmail.com>
2019-12-05 00:19:20 +0800

    upload

--- js/script.js
+++ js/script.js
@@ -33,19 +33,58 @@ $(function(){
             this.$clickNumer=$(".click-number");
             this.$catName = $(".cat-name");
 
+
             this.$catImage.click(function(e){
                  octopus.addClicker();
             });
 
             this.render();
         },
-        render(){
+        render:function() {
             let cat = octopus.getCurrentCat();
-            this.$catImage.attr('src',cat.src);
+            this.$catImage.attr('src', cat.src);
             this.$clickNumer.text(cat.clicker);
             this.$catName.text(cat.name);
         }
+    };
+    let AdminView = {
+        render:function(){
+            let cat = octopus.getCurrentCat();
 
+            $inputName = $("#input-name");
+            $inputClicker = $("#input-clicker");
+            $buttonSave = $("#save");
+            $buttonCancel = $("#cancel");
+            $buttonAdmin = $("#admin");
+
+            //admin view functions
+            console.log("cats",octopus.getAllCats());
+
+
+            // $buttonAdmin.click(function (e) {
+            //     $inputName.val(cat.name);
+            //     $inputClicker.val(cat.clicker);
+            //
+            //     //octopus.toggleAdmin(true);
+            //     console.log("hhhhh");
+            // });
+
+            // $buttonCancel.click(function(e){
+            //     octopus.toggleAdmin(false);
+            // });
+
+            //todo always have been more extra times
+            $buttonSave.click(function (e) {
+                let inputName = $inputName.val(),
+                    inputClicker = $inputClicker.val();
+
+                cat.name = inputName;
+                cat.clicker = inputClicker;
+
+                octopus.setCurrentCat(cat);
+                console.log("cat_item",octopus.getAllCats());
+            });
+        }
     };
 
     let ListView ={
@@ -56,69 +95,42 @@ $(function(){
         },
         render:function() {
             let cats = octopus.getAllCats();
+            this.$listContainer.empty();
 
             for (let i = 0; i < cats.length; i++) {
                 let elem = document.createElement('li');
                 elem.textContent = cats[i].name;
 
-                if(i===0) elem.classList.add("selected");
-
-                this.$listContainer.append(elem);
-
                 elem.addEventListener("click",(function (cat) {
                     return function () {
-                        $('li').removeClass("selected");
-                        $(this).addClass("selected");
                         octopus.setCurrentCat(cat);
                         ImageView.render();
+                        AdminView.render();
                     }
 
                 })(cats[i]));
+
+                this.$listContainer.append(elem);
 
             }
 
         }
     };
 
-    let adminView = {
-        init:function(){
-            $inputName = $("#input-name");
-            $inputClicker = $("#input-clicker");
-            $buttonSave = $("#save");
-            $buttonCancel = $("#cancel");
-
-            let cat = octopus.getCurrentCat();
-            $inputName.val(cat.name);
-            $inputClicker.val(cat.clicker);
-
-            $buttonCancel.click(function(e){
-                $inputName.val(cat.name);
-                $inputClicker.val(cat.clicker);
-            })
-
-            $buttonSave.click(function (e) {
-
-                cat.name = $inputName.val();
-                cat.clicker = $inputClicker.val();
-
-                octopus.adminUpdate(cat);
-                console.log(octopus.getAllCats());
-                //todo submit and vanish the view
-            });
-
-        }
-    }
 
     let octopus = {
         getAllCats:function(){
             return model.data;
         },
+
         adminUpdate: function(cat){
             this.setCurrentCat(cat);
             ImageView.render();
+            ListView.render();
         },
+
         addClicker:function(){
-             ++model.currentCat.clicker;
+             model.currentCat.clicker++;
              ImageView.render();
         },
         getCurrentCat: function(){
@@ -127,11 +139,22 @@ $(function(){
         setCurrentCat:function(cat){
             model.currentCat = cat;
         },
+
+        // toggleAdmin:function(isAdmin){
+        //
+        //     if(isAdmin){
+        //         $(".admin-container").show();
+        //         //adminView.init();
+        //     }else {
+        //         $(".admin-container").hide();
+        //     }
+        //     $("#admin").attr("disabled",isAdmin);
+        // },
         init:function () {
-            this.setCurrentCat(model.data[0]);
+            model.currentCat=model.data[0];
             ListView.init();
             ImageView.init();
-            adminView.init();
+
         }
     };
 
